fd657d343b41d212bf1f42cf4f0bb6bc2a82eb61
KRLoyd <135@holbertonschool.com>
2017-04-12 23:35:10 +0000

    create strelen_rec.c

---
+++ strlen_rec.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+/**
+ * strlen_rec - find the length of a string
+ * @s: string to evaluate
+ *
+ * Return: length of s
+ */
+int strlen_rec(char *s)
+{
+	unsigned int i;
+	if (*s == '\0')
+		return (0);
+	i = (strlen_rec(s + 1));
+	return (i + 1);
+}
