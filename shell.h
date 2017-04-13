4c9ab7c6df6fa313f4740add8030fa4909332c7c
KRLoyd <135@holbertonschool.com>
2017-04-13 18:02:02 +0000

    add header files and defines PROMPT2

--- shell.h
+++ shell.h
@@ -2,6 +2,10 @@
 #define __JKSHELL__
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #define PROMPT "($) "
+#define PROMPT2 "$\n"
 int strlen_rec(char *s);
  #endif /* __JKSHELL__ */
