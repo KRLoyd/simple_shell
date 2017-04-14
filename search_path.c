fb0073218b5437567b952bbab278235ce90396ea
KRLoyd <135@holbertonschool.com>
2017-04-14 21:59:53 +0000

    remove extra buffers

--- search_path.c
+++ search_path.c
@@ -8,8 +8,6 @@
  */
 char *search_path(char *str, list_t *list)
 {
-	char *half_path;
-	char *full_path;
 	char *liststr;
 	int access_res;
 
@@ -18,41 +16,29 @@ char *search_path(char *str, list_t *list)
 	{
 		perror("liststr malloc\n"); return (NULL);
 	}
-	half_path = malloc(sizeof(char) * BUFSIZE);
-	if (half_path == NULL)
-	{
-		perror("half_path malloc\n"); return (NULL);
-	}
-	full_path = malloc(sizeof(char) * BUFSIZE);
-	if (full_path == NULL)
-	{
-		perror("full_path malloc\n"); return (NULL);
-	}
 	while (list != NULL)
 	{
-		clear_buff(full_path);
-		clear_buff(half_path);
 		clear_buff(liststr);
 		liststr = _strcpy(liststr, list->str);
-		half_path = _strcat(liststr, "/");
-		if (half_path == NULL)
+		liststr = _strcat(liststr, "/");
+		if (liststr == NULL)
 		{
-			putstring("Error concatenating to half_path\n");
+			putstring("Error concatenating to liststr\n");
 			return (NULL);
 		}
-		full_path = _strcat(half_path, str);
-		printf("full_path: %s\n", full_path);
-		if (full_path == NULL)
+		liststr = _strcat(liststr, str);
+		printf("full_path: %s\n", liststr);
+		if (liststr == NULL)
 		{
-			putstring("Error concatenating to full_path\n");
+			putstring("Error concatenating to liststr\n");
 			return (NULL);
 		}
 		else
 		{
-			access_res = access(full_path, X_OK);
+			access_res = access(liststr, X_OK);
 			if (access_res == 0)
 			{
-				return (full_path);
+				return (liststr);
 			}
 		}
 		list = list->next;
