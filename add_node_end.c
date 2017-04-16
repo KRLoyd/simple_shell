f72e43920810e57e8beba15c21882b1599ed5226
KRLoyd <loydkristen@gmail.com>
2017-04-16 21:05:18 +0000

    replace putstring with perror

--- add_node_end.c
+++ add_node_end.c
@@ -15,7 +15,7 @@ list_t *add_node_end(list_t **head, char *str)
 	new_last = malloc(sizeof(list_t));
 	if (new_last == NULL)
 	{
-		putstring("Couldn't malloc for new_last\n");
+		perror("Couldn't malloc for new_last\n");
 		return (NULL);
 	}
 	new_last->str = str;
